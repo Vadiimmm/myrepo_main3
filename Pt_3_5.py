58ec8bfdfd0ad0e7b1b3929a12f123f31ce62869
Vadiimmm <vem23@tpu.ru>
2023-07-19 23:13:19 +0300

    fiveth task using List comprehensions

---
+++ Pt_3_5.py
@@ -0,0 +1,15 @@
+import math
+m = int(input("введите кол-во чисел"))
+nums = []
+r = 0
+for i in range(0,m):
+    k = int(input("введите число"))
+    nums.append(k)
+print("начальный список: ", nums)
+for n in nums:
+    for i in range(1,n+1):
+        if n%i == 0:
+            r+=1
+    lst = [n for n in nums if r > 2 ]
+    r = 0
+print("конечный список: ", lst) 
