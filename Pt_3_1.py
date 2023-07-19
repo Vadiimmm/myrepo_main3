4d7f3a76e46085c668a5bee362b4944c800a5f3d
Vadiimmm <vem23@tpu.ru>
2023-07-19 23:05:28 +0300

    first task using lambda function

---
+++ Pt_3_1.py
@@ -0,0 +1,9 @@
+from functools import reduce
+m = int(input("Введите кол-во элементов в списке"))
+a = []
+for i in range(0, m):
+    k = int(input("Введите число"))
+    a.append(k)
+print(a)
+srzn = reduce((lambda x, y: x + y), a)/m
+print(srzn)
