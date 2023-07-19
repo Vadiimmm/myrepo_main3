18e6ff00ba6b008ba8c9f8ddac8d721ccae7e5a5
Vadiimmm <vem23@tpu.ru>
2023-07-19 23:08:22 +0300

    second task using lambda function

---
+++ Pt_3_2.py
@@ -0,0 +1,8 @@
+n = int(input("введи предел"))
+fib = lambda n: fib(n - 1) + fib(n - 2) if n > 2 else 1
+a = []
+for i in range(1,n):
+    if fib(i) > n:
+        break
+    a.append(fib(i))  
+print(a)
