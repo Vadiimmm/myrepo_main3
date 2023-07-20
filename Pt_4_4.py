b9c9afea61ec12b08ba0ca2d0c569c1eb4ae63b3
Vadiimmm <vem23@tpu.ru>
2023-07-21 00:59:04 +0300

    the code compiles all possible permutations and displays those that match the condition

---
+++ Pt_4_4.py
@@ -0,0 +1,21 @@
+import itertools
+
+def find_combinations(numbers, target_sum):
+    combinations = []
+
+    
+    for r in range(1, len(numbers) + 1):
+        for combination in itertools.combinations(numbers, r):
+            if sum(combination) == target_sum:
+                combinations.append(combination)
+
+    combinations = list(set(combinations))
+
+    return combinations
+
+input_numbers = input("Введите список чисел через пробел: ")
+numbers = list(map(int, input_numbers.split()))
+target_sum = int(input("Введите целевую сумму: "))
+
+result = find_combinations(numbers, target_sum)
+print("Уникальные комбинации суммы", target_sum, ":", result)
