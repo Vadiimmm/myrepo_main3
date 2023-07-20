0b94939eed261cef142beeb59a1e9eaaecbd2db8
Vadiimmm <vem23@tpu.ru>
2023-07-21 00:56:00 +0300

    the code checks each number from the range for primeness

---
+++ Pt_4_3.py
@@ -0,0 +1,10 @@
+a = int(input("введите начало диапазона: "))
+b = int(input("введите конец диапазона: "))
+d = 2
+for i in range (a, b+1):
+    while i % d != 0:
+        d += 1
+    if d == i:
+        print(i)
+    d = 2
+    
