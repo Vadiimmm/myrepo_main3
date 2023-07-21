b481cc37c157f0b4811c53698e72024fe3350e9e
Vadiimmm <137765165+Vadiimmm@users.noreply.github.com>
2023-07-21 21:07:22 +0300

    Update Pt_3_8.py

--- Pt_3_8.py
+++ Pt_3_8.py
@@ -1,10 +1,8 @@
-from pprint import pprint 
+from pprint import pprint
 d = dict()
 b = []
 for i in range(97, 123):
-    #d[chr(1)] = format(i, 'b')
     b.append(chr(i))
-for i in range(0,26):
+for i in range(0, 26):
     d[b[i]] = i
 pprint(d)
-    
