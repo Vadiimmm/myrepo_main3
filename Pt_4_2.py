26f938e4b131c1cf53ea1b34e0768386882d4802
Vadiimmm <137765165+Vadiimmm@users.noreply.github.com>
2023-07-21 21:41:08 +0300

    Update Pt_4_2.py

--- Pt_4_2.py
+++ Pt_4_2.py
@@ -2,11 +2,10 @@ def find_palindromes(string):
     palindromes = []
     n = len(string)
 
-    
     for i in range(n):
         for j in range(i + 1, n + 1):
             substring = string[i:j]
-            if substring == substring[::-1] and len(substring) > 1 :
+            if substring == substring[::-1] and len(substring) > 1:
                 palindromes.append(substring)
 
     return palindromes
