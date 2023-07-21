fd5e6424d8f2763675863126f2236f064f12b7d3
Vadiimmm <137765165+Vadiimmm@users.noreply.github.com>
2023-07-21 21:42:41 +0300

    Update Pt_4_3.py
    add spaces and remove blank line

--- Pt_4_3.py
+++ Pt_4_3.py
@@ -1,10 +1,9 @@
 a = int(input("введите начало диапазона: "))
 b = int(input("введите конец диапазона: "))
 d = 2
-for i in range (a, b+1):
+for i in range(a, b + 1):
     while i % d != 0:
         d += 1
     if d == i:
         print(i)
     d = 2
-    
