4f564178d2dea4b7251d100731c09bf9d30a49bf
Vadiimmm <137765165+Vadiimmm@users.noreply.github.com>
2023-07-21 17:34:18 +0300

    Update Pt_1_7.py
    remove tabs and add last line

--- Pt_1_7.py
+++ Pt_1_7.py
@@ -1,10 +1,10 @@
 string1 = input('input string'')
-string2 =''
+string2 = ''
 for char in string1:
-        if char.isupper():
-             string2 += char.lower()
-        elif char.islower():
-            string2 += char.upper()
-        else:
-            string2 += char
+    if char.isupper():
+        string2 += char.lower()
+    elif char.islower():
+        string2 += char.upper()
+    else:
+        string2 += char
 print(string2)
