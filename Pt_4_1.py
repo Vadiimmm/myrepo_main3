c07d5c036588073591ce23fa82c6764f09f05d11
Vadiimmm <137765165+Vadiimmm@users.noreply.github.com>
2023-07-21 21:35:58 +0300

    Update Pt_4_1.py
    add spaces

--- Pt_4_1.py
+++ Pt_4_1.py
@@ -1,11 +1,10 @@
 try:
     num = int(input("Введите число больше 11: "))
     x = [a for a in str(num)]
-    
-    for i in range(len(x)-1):
-        for j in range(len(x)-i-1):
-            if x[j] < x[j+1]:
-                x[j], x[j+1] = x[j+1], x[j]
+    for i in range(len(x) - 1):
+        for j in range(len(x) - i - 1):
+            if x[j] < x[j + 1]:
+                x[j], x[j + 1] = x[j + 1], x[j]
     print(''.join(x))
 except ValueError:
     print("Неверное значение")
