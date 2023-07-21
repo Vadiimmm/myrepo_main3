c4e8afbeae30f671ac9c2c611cb1e3ddfc941f9e
Vadiimmm <137765165+Vadiimmm@users.noreply.github.com>
2023-07-21 18:41:11 +0300

    Delete Pt_3_4.py
    shouldn't be in this branch

--- Pt_3_4.py
+++
@@ -1,8 +0,0 @@
-m = int(input("введите кол-во чисел"))
-nums = []
-for i in range(0,m):
-    k = int(input("введите число"))
-    nums.append(k)
-print("начальный список: ", nums)
-squares = [n*n for n in nums]
-print("конечный список: ",  squares) 
