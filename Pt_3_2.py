154c35f0db0a4629e9bef6d9bae3e5348fe8b405
Vadiimmm <137765165+Vadiimmm@users.noreply.github.com>
2023-07-21 20:26:39 +0300

    Update Pt_3_2.py
    remove spaces and add ignore to lambda function

--- Pt_3_2.py
+++ Pt_3_2.py
@@ -1,8 +1,8 @@
 n = int(input("введи предел"))
-fib = lambda n: fib(n - 1) + fib(n - 2) if n > 2 else 1
+fib = lambda n: fib(n - 1) + fib(n - 2) if n > 2 else 1--ignore=E731
 a = []
-for i in range(1,n):
+for i in range(1, n):
     if fib(i) > n:
         break
-    a.append(fib(i))  
+    a.append(fib(i))
 print(a)
