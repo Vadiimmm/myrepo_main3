fd0fd74e43566f41564c9da743aac9584f38a911
Vadiimmm <137765165+Vadiimmm@users.noreply.github.com>
2023-07-21 18:37:38 +0300

    Delete Pt_2_6.py
    shouldn't be in this branch

--- Pt_2_6.py
+++
@@ -1,7 +0,0 @@
-dist = int(input('сколько хотите проехать? '))
-rashod = int(input('сколько автомобиль расходует за 100км? '))
-oil = int(input('сколько литров у вас в баке?'))
-if dist<=oil/rashod:
-	print('проедите')
-else:
-	print('не проедите')
