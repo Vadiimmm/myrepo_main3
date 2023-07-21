b8ba7616e830c6919768360f89cadb4e05261135
Vadiimmm <137765165+Vadiimmm@users.noreply.github.com>
2023-07-21 21:59:42 +0300

    Delete Pt_2_9.py
    shouldn't be in this branch

--- Pt_2_9.py
+++
@@ -1,7 +0,0 @@
-number = int(input("Введите натуральное число: "))
-number = str(number)
-max_digit = max(number)
-max_digit_position_from_end = len(number) - number.index(max_digit)
-max_digit_position_from_start = number.index(max_digit) + 1
-print(f"Порядковый номер максимальной цифры от конца числа: {max_digit_position_from_end}")
-print(f"Порядковый номер максимальной цифры от начала числа: {max_digit_position_from_start}")
