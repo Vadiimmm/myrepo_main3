b06d01769391b4a827c3ee023c38be632f63dce9
Vadiimmm <137765165+Vadiimmm@users.noreply.github.com>
2023-07-21 20:34:15 +0300

    Update Pt_3_2.py

--- Pt_3_2.py
+++ Pt_3_2.py
@@ -1,5 +1,6 @@
 n = int(input("введи предел"))
-fib = lambda n: fib(n - 1) + fib(n - 2) if n > 2 else 1 --ignore=E731
+fib = lambda n: fib(n - 1) + fib(n - 2) if n > 2 else 1 
+--ignore=E731
 a = []
 for i in range(1, n):
     if fib(i) > n:
