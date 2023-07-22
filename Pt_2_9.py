27f300329d1db62d787a0c3a1f1d0b86d272d827
Vadiimmm <137765165+Vadiimmm@users.noreply.github.com>
2023-07-22 10:00:18 +0300

    Update Pt_2_9.py
    make lines shorter

--- Pt_2_9.py
+++ Pt_2_9.py
@@ -1,7 +1,7 @@
 number = int(input("Введите натуральное число: "))
 number = str(number)
 max_digit = max(number)
-max_digit_position_from_end = len(number) - number.index(max_digit)
-max_digit_position_from_start = number.index(max_digit) + 1
-print(f"Порядковый номер максимальной цифры от конца числа: {max_digit_position_from_end}")
-print(f"Порядковый номер максимальной цифры от начала числа: {max_digit_position_from_start}")
+digit_position_end = len(number) - number.index(max_digit)
+digit_position_start = number.index(max_digit) + 1
+print(f"Порядковый номер макс цифры от конца: {digit_position_end}")
+print(f"Порядковый номер макс цифры от начала: {digit_position_start}")
