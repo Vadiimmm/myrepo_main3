8f93295be7df4297e99b0591df493a2690533d7b
Vadiimmm <137765165+Vadiimmm@users.noreply.github.com>
2023-07-22 09:39:47 +0300

    Update Pt_2_3.py

--- Pt_2_3.py
+++ Pt_2_3.py
@@ -2,13 +2,13 @@ a = int(input('введите число '))
 stra = str(a)
 step = len(stra)
 temp = a
-sum =0
+sum = 0
 while temp > 0:
-      digit = temp % 10
-      sum += digit**step
-      temp //= 10
-if a==sum:
-	print('это число армстронга')
+    digit = temp % 10
+    sum += digit**step
+    temp //= 10
+if a == sum:
+    print('это число армстронга')
 else:
-	print('это  не число армстронга')
+    print('это  не число армстронга')
 	
