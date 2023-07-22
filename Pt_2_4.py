76b2e79871d1aa29f6c1723736d1b0015345f5cf
Vadiimmm <137765165+Vadiimmm@users.noreply.github.com>
2023-07-22 09:43:40 +0300

    Update Pt_2_4.py

--- Pt_2_4.py
+++ Pt_2_4.py
@@ -8,7 +8,7 @@ elif d == 0:
     x = (-b) / (2 * a)
     print(f'x = {x}')
 else:
-    x1 = (-b + d**0.5)/(2 * a)
-    x2 = (-b - d**0.5)/(2 * a)
+    x1 = (-b + d**0.5) / (2 * a)
+    x2 = (-b - d**0.5) / (2 * a)
     print(f'x1 = {x1}')
     print(f'x2 = {x2}')
