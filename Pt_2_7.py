bec3ccb2c300be588ca9bdcf74fa9b5efff0e90a
Vadiimmm <137765165+Vadiimmm@users.noreply.github.com>
2023-07-22 09:53:32 +0300

    Update Pt_2_7.py

--- Pt_2_7.py
+++ Pt_2_7.py
@@ -1,5 +1,5 @@
 sum = int(input('введите отрицательное число '))
-while sum<0:
-	a = int(input('введите число '))
-	sum+=a
-	print(sum)
+while sum < 0:
+    a = int(input('введите число '))
+    sum += a
+    print(sum)
